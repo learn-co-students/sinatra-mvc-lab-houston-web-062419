d2e9ff26df634c4ab9bd7e7fc86c2028b54cf2f6
Varvara Babkova <varvara.munday@gmail.com>
2019-07-16 11:31:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get "/" do 
+		erb :user_input
+	end
+
+	post  "/piglatinize" do
+		# puts params
+		 @result = PigLatinizer.new.piglatinize(params[:user_phrase])
+		# binding.pry
+		erb :result
+	end
 end
